9151d3cc4ba692495067dc534d649979cebd5ec8
nam2709 <9namancam@gmail.com>
2023-10-16 15:29:59 +0700

    Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,51 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Cardlist from '../components/Cardlist';
 import SeacrhBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
 
-    componentDidMount() {
+function App() {
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
+            .then(users => setRobots(users));
+    },[])
+
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
 
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
-
-    render() {
-        const { robots, searchfield } = this.state;
-        const filterRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
-        if (!robots.length){
-            return <h1>Loading</h1>
-        } else {
-            return (
-                <div className='tc'>
-                    <h1 className='f1'> RoboFriends</h1>
-                    <SeacrhBox searchChange={this.onSearchChange}/> 
-                    {/* but this an objects so must include this. before */}
-                    <Scroll>
-                        <ErrorBoundry>
-                            <Cardlist robots={filterRobots}/>
-                        </ErrorBoundry>
-                    </Scroll>
-                </div> 
-            );
-        }  
-    }
+    const filterRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
+    if (!robots.length){
+        return <h1>Loading</h1>
+    } else {
+        return (
+            <div className='tc'>
+                <h1 className='f1'> RoboFriends</h1>
+                <button onClick={() => setCount(count+1) }> Click me </button>
+                <SeacrhBox searchChange={onSearchChange}/> 
+                {/* but this an objects so must include this. before */}
+                <Scroll>
+                    <ErrorBoundry>
+                        <Cardlist robots={filterRobots}/>
+                    </ErrorBoundry>
+                </Scroll>
+            </div> 
+        );
+    }  
+    
 }
 
 export default App
